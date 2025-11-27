d10de48c437d5c13a8586d6473f7f789eaeb5c5f
siddhesh <siddheshneharkar70@gmail.com>
2025-11-27 12:55:07 +0530

    fix: use VITE_API_BASE_URL environment variable

--- frontend/src/stores/eventStore.js
+++ frontend/src/stores/eventStore.js
@@ -1,7 +1,8 @@
 import { create } from 'zustand';
 import axios from 'axios';
 
-const API_BASE = 'http://localhost:5000/api';
+const API_BASE = import.meta.env.VITE_API_BASE_URL + '/api';
+
 
 // Create axios instance with default config
 const api = axios.create({
